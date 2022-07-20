786f580e531894ee36597d8b5a5b05544498870e
Vincent <vincent.makokha@student.moringaschool.com>
2022-07-20 16:30:16 +0300

    Still on the process of creating a js that processes products in cart

--- checkout.js
+++ checkout.js
@@ -1,3 +1,16 @@
 window.onload = ()=>{
-    console.log(localStorage)
+    let checkoutItems = document.querySelector('.checkout .price table');
+    checkoutItems.innerHTML = `<tr>
+    <th><strong>Product<span>s</span></strong></th><th><p>Price/Unit</th><th><p>No. of Units</p></th><th><span>Total</span></p></th>
+
+</tr>
+`
+
+let itemsOnLocalStorage = localStorage.getItem('inCart');
+
+if(itemsOnLocalStorage != null){
+    for([item, price] of Object.entries(itemsOnLocalStorage)){
+        console.log(item)
+    }
+}
 }
